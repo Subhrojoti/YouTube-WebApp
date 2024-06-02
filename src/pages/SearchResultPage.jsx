ce6aea7ac0ab08519f4ae33dde2719359543c2b4
Subhrojoti <subhrojoti20@gmail.com>
2024-06-02 12:17:54 +0530

    create searchPage, VideoDetailsPage, and create routing

--- src/pages/SearchResultPage.jsx
+++ src/pages/SearchResultPage.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import SearchResultVideoCard from "../Components/SearchResultVideoCard";
+import Sidebar from "../Components/SideBar/Sidebar";
+
+import { dummyData } from "../DummyData/dummyData";
 
 const SearchResultPage = () => {
-  return <div>Search Result</div>;
+  return (
+    <div className="flex flex-row">
+      <Sidebar />
+      <div className="grow h-full w-[calc(100%-240px)] overflow-y-auto bg-white">
+        <div className="grid grid-cols-1 gap-2 p-5">
+          {dummyData.map((i, k) => (
+            <SearchResultVideoCard />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SearchResultPage;
